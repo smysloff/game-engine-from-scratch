9b100faaf07995e5f25e556f79946bbcdcdeb76a
smysloff <kokoc.smyslov@yandex.ru>
2026-02-27 16:42:44 +0300

    wip(examples): system

--- examples/system/get_keyboard.c
+++ examples/system/get_keyboard.c
@@ -1,98 +1,38 @@
 
 // file: examples/system/get_keyboard.c
-// compile:
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+#include <string.h>
+#include <ctype.h>
 #include <xcb/xcb.h>
 #include <xcb/xkb.h>
 #include <xkbcommon/xkbcommon-x11.h>
 
-xcb_connection_t *x11_init(void);
-void x11_quit(xcb_connection_t *connection);
-void x11_use_xkb(xcb_connection_t *connection);
-void x11_unuse_xkb(struct xkb_context *context, struct xkb_keymap *keymap, struct xkb_state *state);
-struct xkb_context *x11_xkb_get_context(xcb_connection_t *connection);
-int32_t x11_xkb_get_device_id(xcb_connection_t *connection, struct xkb_context *context);
-struct xkb_keymap *x11_xkb_get_keymap(xcb_connection_t *connection, struct xkb_context *context, int32_t device_id);
-struct xkb_state *x11_xkb_get_state(xcb_connection_t *connection, struct xkb_context *context, int32_t device_id, struct xkb_keymap *keymap);
-
-
-int
-main(void)
+typedef struct x11_xkb_layout_s
 {
-  // variables
-  char layout_name[256];
-
-  // xcb
-  xcb_connection_t *connection;
-
-  // xkbcommon
   struct xkb_context *context;
-  int32_t device;
   struct xkb_keymap *keymap;
   struct xkb_state *state;
-
-  xkb_layout_index_t num_layouts;
-  xkb_layout_index_t current_layout;
-  const char *name;
-
-
-  connection = x11_init();
-
-  x11_use_xkb(connection);
-
-  context = x11_xkb_get_context(connection);
-  device  = x11_xkb_get_device_id(connection, context);
-  keymap  = x11_xkb_get_keymap(connection, context, device);
-  state   = x11_xkb_get_state(connection, context, device, keymap);
-
-
-  // get current keyboard layout name
-
-  num_layouts = xkb_keymap_num_layouts(keymap);
-  current_layout = xkb_state_key_get_layout(state, XKB_STATE_LAYOUT_EFFECTIVE);
-
-  if (current_layout != XKB_LAYOUT_INVALID)
-  {
-    name = xkb_keymap_layout_get_name(keymap, current_layout);
-
-    (name == NULL)
-      ? printf("layout: (%d of %lu)\n", current_layout, (unsigned long) num_layouts)
-      : printf("layout: %s (%d of %lu)\n", name, current_layout, (unsigned long) num_layouts);
-  }
-
-  else
-    printf("layout: unknown\n");
-
-
-  // cleanup and exit
-
-  x11_unuse_xkb(context, keymap, state);
-  x11_quit(connection);
-
-  return EXIT_SUCCESS;
-}
+  int32_t device_id;
+} x11_xkb_layout_t;
 
 
 xcb_connection_t *
 x11_init(void)
 {
   xcb_connection_t *connection;
-  if (xcb_connection_has_error(connection))
-  {
-    fprintf(stderr, "Cannot connect to X-server\n");
-    exit(EXIT_FAILURE);
-  }
+  connection = xcb_connect(NULL, NULL);
+  assert(!xcb_connection_has_error(connection));
   return connection;
 }
 
 void
 x11_quit(xcb_connection_t *connection)
 {
-  if (connection)
-    xcb_disconnect(connection);
+  assert(connection);
+  xcb_disconnect(connection);
 }
 
 void
@@ -101,97 +41,124 @@ x11_use_xkb(xcb_connection_t *connection)
   xcb_xkb_use_extension_cookie_t cookie;
   xcb_xkb_use_extension_reply_t *reply;
 
-  cookie = xcb_xkb_use_extension(
-    connection, XCB_XKB_MAJOR_VERSION, XCB_XKB_MINOR_VERSION);
+  assert(connection);
 
+  cookie = xcb_xkb_use_extension(connection, XCB_XKB_MAJOR_VERSION, XCB_XKB_MINOR_VERSION);
   reply = xcb_xkb_use_extension_reply(connection, cookie, NULL);
 
-  if (!reply || !reply->supported)
-  {
-    free(reply);
-    fprintf(stderr, "error: xcb_xkb_use_extension_reply()\n");
-    x11_quit(connection);
-    exit(EXIT_FAILURE);
-  }
-
+  assert(reply && reply->supported);
   free(reply);
 }
 
+x11_xkb_layout_t *
+x11_xkb_layout_init(xcb_connection_t *connection)
+{
+  x11_xkb_layout_t *layout;
+
+  assert(connection);
+
+  layout = malloc(sizeof(x11_xkb_layout_t));
+  assert(layout);
+
+  layout->context = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
+  assert(layout->context);
+
+  layout->device_id = xkb_x11_get_core_keyboard_device_id(connection);
+  assert(layout->device_id != -1);
+
+  layout->keymap = xkb_x11_keymap_new_from_device(
+    layout->context, connection, layout->device_id, XKB_KEYMAP_COMPILE_NO_FLAGS);
+  assert(layout->keymap);
+
+  layout->state = xkb_x11_state_new_from_device(
+    layout->keymap, connection, layout->device_id);
+  assert(layout->state);
+
+  return layout;
+}
+
 void
-x11_unuse_xkb(
-  struct xkb_context *context,
-  struct xkb_keymap *keymap,
-  struct xkb_state *state
-) {
-  xkb_state_unref(state);
-  xkb_keymap_unref(keymap);
-  xkb_context_unref(context);
-}
-
-struct xkb_context *
-x11_xkb_get_context(xcb_connection_t *connection)
+x11_xkb_layout_free(x11_xkb_layout_t *layout)
 {
-  struct xkb_context *context;
-  if (!(context = xkb_context_new(XKB_CONTEXT_NO_FLAGS)))
-  {
-    fprintf(stderr, "error: xkb_context_new()\n");
-    x11_quit(connection);
-    exit(EXIT_FAILURE);
-  }
-  return context;
+  assert(layout);
+  xkb_state_unref(layout->state);
+  xkb_keymap_unref(layout->keymap);
+  xkb_context_unref(layout->context);
+  free(layout);
 }
 
-int32_t
-x11_xkb_get_device_id(xcb_connection_t *connection, struct xkb_context *context)
+const char *
+x11_xkb_layout_get_fullname(
+  xcb_connection_t *connection,
+  x11_xkb_layout_t *layout
+) {
+  const char *fullname;
+  xkb_layout_index_t num_layouts; // @todo
+  xcb_xkb_get_state_cookie_t cookie;
+  xcb_xkb_get_state_reply_t *reply;
+
+  assert(connection);
+  assert(layout);
+
+  num_layouts = xkb_keymap_num_layouts(layout->keymap);
+  cookie = xcb_xkb_get_state(connection, layout->device_id);
+  reply = xcb_xkb_get_state_reply(connection, cookie, NULL);
+  assert(reply);
+  fullname = xkb_keymap_layout_get_name(layout->keymap, reply->lockedGroup);
+  free(reply);
+
+  return fullname;
+}
+
+const char *
+x11_xkb_layout_get_shortname(
+  xcb_connection_t *connection,
+  x11_xkb_layout_t *layout
+) {
+  static char shortname[3];
+  const char *fullname;
+
+  assert(connection);
+  assert(layout);
+
+  fullname = x11_xkb_layout_get_fullname(connection, layout);
+
+  if (fullname && strlen(fullname) >= 2)
+  {
+    shortname[0] = tolower(fullname[0]);
+    shortname[1] = tolower(fullname[1]);
+  }
+  else
+  {
+    shortname[0] = '?';
+    shortname[1] = '?';
+  }
+  shortname[2] = '\0';
+
+  return shortname;
+}
+
+
+int
+main(void)
 {
-  int32_t id;
-  if ((id = xkb_x11_get_core_keyboard_device_id(connection)) == -1)
-  {
-    fprintf(stderr, "error: xkb_x11_get_core_keyboard_device()\n");
-    xkb_context_unref(context);
-    xcb_disconnect(connection);
-    exit(EXIT_FAILURE);
-  }
-  return id;
-}
+  xcb_connection_t *connection;
+  x11_xkb_layout_t *layout;
+  const char *layout_shortname;
+  const char *layout_fullname;
 
-struct xkb_keymap *
-x11_xkb_get_keymap(
-  xcb_connection_t *connection,
-  struct xkb_context *context,
-  int32_t device_id
-) {
-  struct xkb_keymap *keymap;
+  connection = x11_init();
+  x11_use_xkb(connection);
+  layout = x11_xkb_layout_init(connection);
 
-  keymap = xkb_x11_keymap_new_from_device(
-    context, connection, device_id, XKB_KEYMAP_COMPILE_NO_FLAGS);
+  layout_shortname = x11_xkb_layout_get_shortname(connection, layout);
+  layout_fullname = x11_xkb_layout_get_fullname(connection, layout);
 
-  if (!keymap)
-  {
-    fprintf(stderr, "error: xkb_x11_keymap_new_from_device()\n");
-    xkb_context_unref(context);
-    xcb_disconnect(connection);
-    exit(EXIT_FAILURE);
-  }
+  printf("current layout: %s (%s)\n",
+    layout_shortname, layout_fullname ? layout_fullname : "Unknown");
 
-  return keymap;
-}
+  x11_xkb_layout_free(layout);
+  x11_quit(connection);
 
-struct xkb_state *
-x11_xkb_get_state(
-  xcb_connection_t *connection,
-  struct xkb_context *context,
-  int32_t device_id,
-  struct xkb_keymap *keymap
-) {
-  struct xkb_state *state;
-  if (!(state = xkb_x11_state_new_from_device(keymap, connection, device_id)))
-  {
-    fprintf(stderr, "error: xkb_x11_keymap_new_from_device\n");
-    xkb_keymap_unref(keymap);
-    xkb_context_unref(context);
-    xcb_disconnect(connection);
-    exit(EXIT_FAILURE);
-  }
-  return state;
+  return EXIT_SUCCESS;
 }
